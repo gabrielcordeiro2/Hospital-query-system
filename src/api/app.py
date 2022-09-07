b5f7cb855dcb56b41b17187ce8971d1311304e6e
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-09-07 20:53:56 -0300

    update: change database to PostgreSQL

--- src/api/app.py
+++ src/api/app.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from models.paciente import PacienteModel
-from models.consulta import ConsultaModel
-from models.internacao import InternacaoModel
+from models import ConsultaModel, InternacaoModel, PacienteModel, UserModel
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if __name__ == "__main__":
-    from config.orm_banco import banco
+    from config import banco
     banco.init_app(app)
     banco.create_all(app=app)
+    app.run(debug=True)
