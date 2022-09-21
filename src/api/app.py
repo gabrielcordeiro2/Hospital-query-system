8349d32169aefcc96795a879a4b4ba6b9375841e
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-09-21 04:04:19 -0300

    feat: add POST and PUT Patient methods

--- src/api/app.py
+++ src/api/app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from globals import importResource
 from config.database import Base, db_config
 from models.entities import *
-from resources.patient import Patient
+from resources.patient import Patient, PatientRegister
 
 app = Flask(__name__)
 
@@ -13,8 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 conn = importResource('conn', 'config.database_instance')
 api.add_resource(Patient, '/patient/<string:info>')
+api.add_resource(PatientRegister, '/patient/register')
 
 if __name__ == "__main__":
 
     Base.metadata.create_all(conn.create_engine()) # Create DB
-    app.run(debug=False)
+    app.run(debug=True)
--- src/api/resources/patient.py
+++ src/api/resources/patient.py
@@ -3,30 +3,171 @@ from flask_restful import Resource
 from globals import importResource
 from repository.patient import PatientRepository
 
+from flask_restful import reqparse
+
 conn = importResource('conn', 'config.database_instance')
 
 class Patient(Resource):
-    ''' Patient endpoint methods '''
+    ''' Find or Update Patients info endpoint methods '''
+    arguments = reqparse.RequestParser()
+    arguments.add_argument('name', type=str, required=True, help="The field 'name' cannot be left blank.")
+    arguments.add_argument('cpf')
+    arguments.add_argument('birthdate')
+    arguments.add_argument('phone')
+    arguments.add_argument('sus_card')
+
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         print('Patient was instantiated')
         self.repo = PatientRepository(conn)
 
-    def get(self, info):
-        response1 = self.repo.find_patient(info)
-
+    def get(self, info: str):
+        response = self.repo.find_patient(info)
         result = []
-        for patient in response1:
+
+        for patient in response:
             json_patient = {
                 'name': patient.name,
-                'nascimento': str(patient.birthdate),
+                'birthdate': str(patient.birthdate),
                 'cpf': patient.cpf,
-                'telefone': patient.phone,
-                'cartao_sus': patient.sus_card
+                'phone': patient.phone,
+                'sus_card': patient.sus_card
             }
             result.append(json_patient)
-        return result
 
+        if result:
+            return result
+        return {'message': 'User not found'}, 404
+
+    def put(self, info):
+        data = self.arguments.parse_args()
+        patient_found = self.repo.find_patient(info)
+        print(patient_found)
+
+        if patient_found:
+            self.repo.update_patient_info(info, **data)
+            return {'message': 'Patient updated successfully!'}, 200
+        return {"message": "Patient not found."}, 404
+
+class PatientRegister(Resource):
+    ''' Create Patient endpoint method '''
+    arguments = reqparse.RequestParser()
+    arguments.add_argument('name', type=str, required=True, help="The field 'name' cannot be left blank.")
+    arguments.add_argument('cpf')
+    arguments.add_argument('birthdate')
+    arguments.add_argument('phone')
+    arguments.add_argument('sus_card')
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        print('Patient was instantiated')
+        self.repo = PatientRepository(conn)
+
+    def post(self):
+        data = self.arguments.parse_args()
+        cpf_found = self.repo.find_patient(data.get('cpf'))
+        sus_card_found = self.repo.find_patient(data.get('sus_card'))
+
+        if cpf_found:
+            return {
+                "message": f"Patient with cpf '{data.get('cpf')}' already exists."
+            }, 400
+
+        if sus_card_found:
+            return {
+                "message": f"Patient with sus_card '{data.get('sus_card')}' already exists."
+            }, 400
+
+        self.repo.register_patient(**data)
+        return {'message': 'Patient created successfully!'}, 201
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def get(self, info): # all patients and his appointments method 1
+    #     ''' Get all patients with "info" and his appointments '''
+    #     response_patient = self.repo.find_patient(info)
+    #     response_appointments = self.repo.find_appointments(info)
+
+    #     result = []
+    #     for patient in response_patient:
+
+    #         appointments = []
+    #         for appointment in response_appointments:
+
+    #             json_appointment = {
+    #                 'patient_name': appointment.patient_name,
+    #                 'appointment_date': str(appointment.appointment_date),
+    #                 'patient_attended': appointment.patient_attended,
+    #                 'doctor_name': appointment.doctor_name
+    #             }
+    #             appointments.append(json_appointment)
+
+    #         json_info = {
+    #             'name': patient.name,
+    #             'birthdate': str(patient.birthdate),
+    #             'cpf': patient.cpf,
+    #             'phone': patient.phone,
+    #             'sus_card': patient.sus_card,
+    #             'appointments': appointments
+    #         }
+    #         result.append(json_info)
+
+    #     if result:
+    #         return result
+    #     return {'message': 'user not found'}, 404
+
+    # def get(self, info): # all patients and his appointments method 2
+    #     ''' Get all patients with "info" and his appointments '''
+    #     response_patient = self.repo.find_patient(info)
+    #     response_appointments = self.repo.find_appointments(info)
+
+    #     result = []
+    #     appointments = []
+
+    #     for appointment in response_appointments:
+    #         json_appointment = {
+    #             'patient_name': appointment.patient_name,
+    #             'appointment_date': str(appointment.appointment_date),
+    #             'patient_attended': appointment.patient_attended,
+    #             'doctor_name': appointment.doctor_name
+    #         }
+    #         appointments.append(json_appointment)
+
+    #     for patient in response_patient:
+    #         json_patient = {
+    #             'name': patient.name,
+    #             'birthdate': str(patient.birthdate),
+    #             'cpf': patient.cpf,
+    #             'phone': patient.phone,
+    #             'sus_card': patient.sus_card,
+    #             'appointments': appointments
+    #         }
+    #         result.append(json_patient)
+
+    #     if result:
+    #         return result
+    #     return {'message': 'user not found'}, 404
 
 
 
