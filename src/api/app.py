425d370d5651de4c5eea7cfc6a68180672f5b7ff
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-09-29 00:29:41 -0300

    fix: fix database connection

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from globals import importResource
-from config.database import Base, db_config
+from config.database import Base, db_config, DBConnection
 from models.entities import *
 from resources.patient import Patient, PatientRegister
 from resources.appointment import Appointment, AppointmentRegister, AppointmentUpdate
@@ -12,14 +11,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_config["URI"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 api = Api(app)
-conn = importResource('conn', 'config.database_instance')
 api.add_resource(Patient, '/patient/<string:info>')
 api.add_resource(PatientRegister, '/patient/register')
 api.add_resource(Appointment, '/appointment/<string:info>')
 api.add_resource(AppointmentRegister, '/appointment/schedule')
-api.add_resource(AppointmentUpdate, '/appointment/<string:appointment_id>')
+api.add_resource(AppointmentUpdate, '/appointment/update/<string:appointment_id>')
 
 if __name__ == "__main__":
-
-    Base.metadata.create_all(conn.create_engine()) # Create DB
+    Base.metadata.create_all(DBConnection().create_engine()) # Create DB
     app.run(debug=True)
