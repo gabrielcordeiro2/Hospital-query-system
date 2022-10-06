716910544b614a570f44c661a58421f2aba3e003
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-10-05 21:23:42 -0300

    config: adjust DBConnection_handler

--- src/api/repository/employee.py
+++ src/api/repository/employee.py
@@ -16,31 +16,25 @@ class EmployeeRepository:
         return doctor
 
     def register_user(self, name, user, password):
-        with DBConnection() as db:
-            user_info = UserModel(
-                name=name,
-                user=user,
-                password=password,
-                type='attendant'
-            )
-            db.session.add(user_info)
-            db.session.commit()
+        user_info = UserModel(
+            name=name,
+            user=user,
+            password=password,
+            type='attendant'
+        )
+        self.db.session.add(user_info)
+        self.db.session.commit()
 
     def find_by_login(self, user):
-        # Está sendo usado no registro
-        with DBConnection() as db:
-            query = db.session\
-                .query(UserModel.user)\
-                .filter(UserModel.user == user)\
-                .first()
-            return query
+        query = self.db.session\
+            .query(UserModel.user)\
+            .filter(UserModel.user == user)\
+            .first()
+        return query
 
     def find_password(self, user):
-        # vai ser utilizado no login
-        # vai encontrar o user e o password com base no user
-        with DBConnection() as db:
-            query = db.session\
-                .query(UserModel.password)\
-                    .filter(UserModel.user == user)\
-                    .first()
-            return query
+        query = self.db.session\
+            .query(UserModel.password)\
+                .filter(UserModel.user == user)\
+                .first()
+        return query
